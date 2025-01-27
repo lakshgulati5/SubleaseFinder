a1ad5d35f190b7409aa28543b611de6d62d1f609
Laksh Gulati <gulatilaksh@outlook.com>
2025-01-26 23:29:43 -0600

    intialize springboot

---
+++ subleasefinder/src/main/java/com/sublease/subleasefinder/SubleasefinderApplication.java
@@ -0,0 +1,13 @@
+package com.sublease.subleasefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubleasefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubleasefinderApplication.class, args);
+	}
+
+}
---
+++ subleasefinder/src/test/java/com/sublease/subleasefinder/SubleasefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sublease.subleasefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubleasefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
